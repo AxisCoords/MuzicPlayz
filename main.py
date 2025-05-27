527d9cb9b08f2637d86d50ec275036eb8b9b23b6
Axis <themid.hamid@gmail.com>
2025-05-27 12:01:20 +0200

    e

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import pygame, sys
+import pygame, sys, json
 import tkinter as tk
 from tkinter import filedialog
 import button
 
+pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.init()
-pygame.mixer.init()
 
-pygame.display.set_caption("MuzicPlaiz")
+pygame.display.set_caption("MuzicPlayz")
 screen = pygame.display.set_mode((400, 400))
 clock = pygame.time.Clock()
 
@@ -14,8 +14,19 @@ icons = pygame.Surface((256, 256))
 icons.fill((0, 0, 0))
 pygame.display.set_icon(icons)
 
-openFileBtn: button.Button = button.Button(pygame.Rect((400 - 160) / 2, ((400 - 30) / 2) + 60, 160, 30), 4)
-playBtn: button.Button = button.Button(pygame.Rect(0, 0, 40, 40), 20)
+DATA_FILE_PATH = "data.json"
+dataObj = {
+    "paused": True,
+    "directory": ""
+}
+try:
+    with open(DATA_FILE_PATH, "r") as file:
+        dataObj = json.load(file)
+except:
+    with open(DATA_FILE_PATH, "w") as file:
+        json.dump(dataObj, file, indent=4)
+
+openFileBtn: button.Button = button.Button(pygame.Rect((400 - 180) / 2, 400 - 30 - 15, 180, 30), 3)
 
 def selectFolder() -> str:
     root = tk.Tk()
@@ -31,19 +42,19 @@ while running:
     events = pygame.event.get()
     
     for event in events:
-        if event.type == pygame.QUIT or event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
-            running = False
+        if event.type == pygame.QUIT or event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE: running = False
     
     if openFileBtn.isClicked(events):
-        directory = selectFolder()
-        if len(directory) > 0: print(directory)
-        else: print("[WARNING]: Didn't select any folder")
+        dataObj["directory"] = selectFolder()
+        print("[INFO]: Chosen directory '" + dataObj["directory"] + "'")
 
-    openFileBtn.draw(screen, "Hello World!", pygame.Color(255, 255, 255), pygame.Color(0, 0, 0))
-    playBtn.draw(screen, "I I", pygame.Color(255, 255, 255), pygame.Color(0, 0, 0))
+    openFileBtn.draw(screen, "OPEN FOLDER", pygame.Color(255, 255, 255), pygame.Color(0, 0, 0))
 
     pygame.display.update()
     clock.tick(60)
+
+with open(DATA_FILE_PATH, "w") as file:
+    json.dump(dataObj, file, indent=4)
 
 pygame.quit()
 sys.exit()
